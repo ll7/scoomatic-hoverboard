b19d221a32c26395b5a8aa87ecbb3b45520b4409
Henri Chilla <henri.chilla@student.uni-augsburg.de>
2019-11-27 14:16:38 +0100

    migrated first node

--- code/ros-drivers/scoomatic_ros1/src/motor_driver.py
+++ code/ros-drivers/scoomatic_ros1/src/motor_driver.py
@@ -9,25 +9,73 @@
 
 import serial
 import rospy
+from time import sleep
 from geometry_msgs.msg import Twist
 
+last_bytes = bytearray([0, 0, 0, 0])
 
+# Get parameter from launchfile or use default value
 def get_param(param, default_value):
-    if rospy.has_param('serial_port'):
-        ser_port=rospy.get_param('serial_port')
+    if rospy.has_param(param):
+        return rospy.get_param(param)
     else:
         rospy.logwarn("No param set for %s , using default value (%s)" % (param, default_value) )
+        return default_value
 
+# Callback for subscriber to /cmd_vel
+# Receives geometry_msgs/Twist message
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    global last_bytes, node
+    #    node.get_logger().info(
+    #        'I heard: "%s"' % data.linear.x)
+    #    node.get_logger().info("Callback called back")
+    # update stored command value
+    last_bytes = twist2bytes(data)
 
-def motor():
+# Sends the actual data to the serial port
+def send_serial(ser):
+    ser.write(last_bytes)
+
+# Limits value to +- max
+def limit(value, limit):
+    return min(limit, max(-limit, value))
+
+# Creates serial data packet for hoverboard from
+# geometry_msgs/Twist message
+def twist2bytes(message):
+    # Limit max and min value to 1000
+    linear_velocity = limit(message.linear.x * 1000, 1000)
+    angular_velocity = limit(message.angular.z * 1000, 1000)
+
+    # reduce maximum steering speed
+    angular_velocity *= 0.66
+
+    # convert to int
+    linear_velocity = int(linear_velocity)
+    angular_velocity = int(angular_velocity)
+
+    # Create data packet for the serial port
+
+    var = 0
+    return angular_velocity.to_bytes(2, byteorder='little', signed=True) + linear_velocity.to_bytes(2,
+                                                                                                    byteorder='little',
+                                                                                                    signed=True)
+
+def main():
+    # start subscriber node
     rospy.init_node('motor_driver', anonymous=True)
-    rospy.Subscriber('cmd_vel', Twist, queue_size=10)
+    rospy.Subscriber('cmd_vel', Twist, callback , queue_size=10)
+    ser_port=get_param('ser_port', '/dev/motor_driver')
 
-    rospy.spin()
-    rospy.loginfo(ser_port)
+    rospy.loginfo("Motor Driver Online on %s" % (ser_port))
 
+    sleeptime = 1 / 50
+
+    with serial.Serial(ser_port, 19200) as ser:
+        while not rospy.is_shutdown():
+            send_serial(ser)
+            sleep(sleeptime)
+            rospy.spin()
         
 if __name__ == '__main__':
-    motor()
+    main()
