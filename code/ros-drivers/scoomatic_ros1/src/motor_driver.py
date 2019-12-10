087a50c98f46fde2e63bea3ddd33c2ca20bf6bbd
Henri Chilla <henri.chilla@student.uni-augsburg.de>
2019-12-10 13:21:36 +0100

    [NEW MOTORDRIVER] test via struct.pack & Serial

--- code/ros-drivers/scoomatic_ros1/src/motor_driver.py
+++ code/ros-drivers/scoomatic_ros1/src/motor_driver.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python
 
+import time
 import serial
 import struct
 from geometry_msgs.msg import Twist
 
 def main():
     serial_port = '/dev/motor_driver'
-    baudrate = 19200
-    ser = serial.Serial(serial_port)
-    print(ser.name)
-    generated_bytes = struct.pack('<hh', 0, 500)
-    ser.write(generated_bytes)
-    ser.write(generated_bytes)
-    ser.write(generated_bytes)
-    ser.write(generated_bytes)
-    ser.write(generated_bytes)
-    ser.close()
+
+    rate = 10
+    freq = 1/rate
+    with serial.Serial(serial_port, 19200) as ser:
+        for i in range(0,500):
+            print(ser.name)
+            
+            generated_bytes = struct.pack('<hh', 0, i)
+            time.sleep(freq)
+            ser.write(generated_bytes)
+        for i in range(500,0):
+            print(ser.name)
+            
+            generated_bytes = struct.pack('<hh', 0, i)
+            time.sleep(freq)
+            ser.write(generated_bytes)
 
 
 
